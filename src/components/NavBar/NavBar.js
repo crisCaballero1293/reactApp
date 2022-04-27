7716dea132b8330dae3b828254f0d85c8c1e6d85
Cris Caballero <criscaballero1293@gmail.com>
2022-04-27 16:32:33 -0500

    Segundo desafio crear navBar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+export default function NavBar() {
+  return (
+    <header>
+      <div className="container">
+        <div className="containerLogo">
+          <p className="logo">LOGO</p>
+        </div>
+        <nav className="mainNav">
+          <ul className="listItems">
+            <li>INICIO</li>
+            <li>PRODUCTOS</li>
+            <li>CONTACTO</li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+}
